f0847374899570c1d646f63f30b9bf844db584ab
alain-kubwayo <ahkubwayo@gmail.com>
2023-05-09 13:03:49 +0200

    feat: add navbar

---
+++ src/components/layout/Navbar.jsx
@@ -0,0 +1,20 @@
+import PageWrapper from "./PageWrapper";
+
+const Navbar = () => {
+    return ( 
+        <nav>
+            <PageWrapper>
+                <div className="flex justify-between items-center">
+                    <h1 className="relative text-[#5F6AF6] font-extrabold text-2xl py-4 pr-0 pl-2 before:content-[''] before:absolute before:left-[40%] before:bottom-[16px] before:-z-10 before:w-[100px] before:h-[10px] before:bg-indigo-300/50 before:transform before:-skew-x-12 before:-translate-x-1/2">
+                        Ticket Haul
+                    </h1>
+                    <div>
+                        <button className="bg-[#5F6AF6] py-1 px-4 text-white rounded-md">Cart</button>
+                    </div>
+                </div>
+            </PageWrapper>
+        </nav>
+    );
+}
+ 
+export default Navbar;
